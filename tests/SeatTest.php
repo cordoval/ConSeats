6723bf22b0c14805afe2f85d7b7c07ae44b2d17a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-02-27 15:20:23 -0500

    Refactor tests to use the @depends annotation to make the dependency of isAvailabe() on isReserved() explicit.

--- tests/SeatTest.php
+++ tests/SeatTest.php
@@ -6,20 +6,22 @@ use ConSeats\Domain\Seat;
 class SeatTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @covers ConSeats\Domain\Seat::isAvailable
-     */
-    public function testIsInitiallyAvailable()
-    {
-        $seat = new Seat;
-        $this->assertTrue($seat->isAvailable());
-    }
-
-    /**
      * @covers ConSeats\Domain\Seat::isReserved
      */
     public function testIsInitiallyNotReserved()
     {
         $seat = new Seat;
         $this->assertFalse($seat->isReserved());
+
+        return $seat;
+    }
+
+    /**
+     * @covers  ConSeats\Domain\Seat::isAvailable
+     * @depends testIsInitiallyNotReserved
+     */
+    public function testIsInitiallyAvailable(Seat $seat)
+    {
+        $this->assertTrue($seat->isAvailable());
     }
 }
