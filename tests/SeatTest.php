2e0d2426eba44bd99b4537beaf4149aee1c50bec
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-02-27 15:32:01 -0500

    Add more tests.

--- tests/SeatTest.php
+++ tests/SeatTest.php
@@ -24,4 +24,46 @@ class SeatTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertTrue($seat->isAvailable());
     }
+
+    /**
+     * @covers  ConSeats\Domain\Seat::reserve
+     * @depends testIsInitiallyNotReserved
+     */
+    public function testCanBeReserved(Seat $seat)
+    {
+        $seat->reserve();
+        $this->assertTrue($seat->isReserved());
+
+        return $seat;
+    }
+
+    /**
+     * @covers            ConSeats\Domain\Seat::reserve
+     * @depends           testCanBeReserved
+     * @expectedException ConSeats\Domain\Exception
+     */
+    public function testReservingAnAlreadyReservedSeatRaisesAnException(Seat $seat)
+    {
+        $seat->reserve();
+    }
+
+    /**
+     * @covers  ConSeats\Domain\Seat::cancelReservation
+     * @depends testCanBeReserved
+     */
+    public function testCancellingAReservedSeatWorks(Seat $seat)
+    {
+        $seat->cancelReservation();
+        $this->assertFalse($seat->isReserved());
+    }
+
+    /**
+     * @covers            ConSeats\Domain\Seat::cancelReservation
+     * @depends           testIsInitiallyNotReserved
+     * @expectedException ConSeats\Domain\Exception
+     */
+    public function testTryingToCancelASeatThatIsNotReservedRaisesAnException(Seat $seat)
+    {
+        $seat->cancelReservation();
+    }
 }
