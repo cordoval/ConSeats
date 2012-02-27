2e652458b68c263b5bdff6b112ca5bf7a54a6288
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-02-27 15:57:51 -0500

    Initial work on tests for Event class.

---
+++ tests/EventTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace ConSeats\Domain\Tests;
+
+use ConSeats\Domain\Event;
+
+class EventTest extends \PHPUnit_Framework_TestCase
+{
+    protected $event;
+    protected $room;
+
+    protected function setUp()
+    {
+        $this->room = $this->getMockBuilder('ConSeats\\Domain\\Room')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+
+        $this->event = new Event(
+          'Advanced PHP Training', '2012-02-27', $this->room
+        );
+    }
+
+    /**
+     * @covers ConSeats\Domain\Event::__construct
+     * @covers ConSeats\Domain\Event::getDate
+     * @covers ConSeats\Domain\Event::getName
+     */
+    public function testIsCorrectlyConstructed()
+    {
+        $this->assertEquals(
+          'Advanced PHP Training', $this->event->getName()
+        );
+
+        $this->assertEquals('2012-02-27', $this->event->getDate());
+    }
+
+    /**
+     * @covers ConSeats\Domain\Event::setName
+     */
+    public function testNameCanBeChanged()
+    {
+        $this->event->setName('Very Advanced PHP Training');
+
+        $this->assertEquals(
+          'Very Advanced PHP Training', $this->event->getName()
+        );
+    }
+}
