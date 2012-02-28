a5752b46f31afcdb1e97e95d3822471bf017153e
Stefan Priebsch <stefan@priebsch.de>
2012-02-28 14:46:53 +0100

    Factory uses configuration.

--- src/Factory.php
+++ src/Factory.php
@@ -4,11 +4,16 @@ namespace ConSeats
 {
     class Factory implements FactoryInterface
     {
+        public function __construct(Configuration $configuration)
+        {
+            $this->configuration = $configuration;
+        }
+    
         public function getInstanceFor($type)
         {
             switch (strtolower($type)) {
                 case 'persistence':
-                    return new ConSeats\Backends\EventStore('/tmp');
+                    return new ConSeats\Backends\EventStore($this->configuration->get('dataDir'));
             }
         }
     }
